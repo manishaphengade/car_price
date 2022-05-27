f7d467755fe1cb82552f131d1ef8fc922d9b392c
manishaphengade <manishap730@gmail.com>
2022-05-27 15:33:43 +0530

    added one api called predict also added data.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from flask import Flask 
 
 app = Flask(__name__)
-def index('/'):
+
+@app.route('/')
+def index():
     return 'Welcome api'
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    return 'prediction'
+
 
 if __name__=='__main__':
     app.run()
